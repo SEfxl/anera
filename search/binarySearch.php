2826ac2a847aa3ba727e88396581bc5e8e12721e
fuxinliang <fuxinliang@zuoyebang.com>
2021-09-07 23:24:56 +0800

    二分查找

--- search/binarySearch.php
+++ search/binarySearch.php
@@ -8,5 +8,98 @@
 
 class binarySearch
 {
+    /**
+     * 二分查找
+     * @param $data
+     * @param $target
+     *
+     * @return int
+     */
+    public function binSearch($data, $target) {
 
+        $length = count($data);
+        $left = 0;
+        $right = $length - 1;
+
+        while ($left <= $right) {
+            $mid = $left + intval(($right - $left) / 2);
+            if ($data[$mid] == $target) {
+                return $mid;
+            } elseif ($target > $data[$mid]) {
+                $left = $mid + 1;
+            } elseif ($target < $data[$mid]) {
+                $right = $mid - 1;
+            }
+        }
+
+        return -1;
+
+    }
+
+    public function binSearchLeft($data, $target) {
+
+        $length = count($data);
+        $left = 0;
+        $right = $length -1;
+
+        while ($left <= $right) {
+            $mid = $left + intval(($right - $left) / 2);
+            if($data[$mid] == $target) {
+              $right = $mid-1;
+            } elseif ($target > $data[$mid]) {
+                $left = $mid + 1;
+            }elseif ($target < $data[$mid]) {
+                $right = $mid - 1;
+            }
+        }
+
+        if($left >= $length || $data[$left] != $target) {
+            return -1;
+        }
+
+        return $left;
+    }
+
+    public function binSearchRight($data, $target) {
+
+        $length = count($data);
+        $left = 0;
+        $right = $length - 1;
+
+        while ($left <= $right) {
+
+            $mid = $left + intval(($right - $left) / 2);
+
+            if($data[$mid] == $target) {
+                $left = $mid + 1;
+            } elseif ($target > $data[$mid]) {
+                $left = $mid + 1;
+            }elseif ($target < $data[$mid]) {
+                $right = $mid - 1;
+            }
+        }
+
+        if($right < 0 || $data[$right] != $target) {
+
+            return -1;
+        }
+
+        return $right;
+
+    }
 }
+
+
+$obj = new binarySearch();
+
+//$data = [1,2,3,4,5,6,7,8,9];
+//$result = $obj->binSearch($data,1);
+//echo "search result: ".$result.PHP_EOL;
+
+//$data2 = [1,2,2,2,3];
+//$res = $obj->binSearchLeft($data2,1);
+//echo "left index = ".$res.PHP_EOL;
+
+$data2 = [1,2,2,2,3];
+$res = $obj->binSearchRight($data2,2);
+echo "right index = ".$res.PHP_EOL;
