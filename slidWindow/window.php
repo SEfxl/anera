df8c07b5a8311efc3e01618e553e05e3a11d2d34
fuxinliang <fuxinliang@zuoyebang.com>
2021-09-08 17:46:26 +0800

    add

---
+++ slidWindow/window.php
@@ -0,0 +1,244 @@
+<?php
+/**
+ * @Desc:
+ * @Author
+ * @Time:  2:17 下午
+ */
+
+
+class window
+{
+    /**
+     * 最小覆盖子串:在字符串s中找出包含t所有字母的最小子串
+     * @param $s
+     * @param $t
+     * @return bool|string
+     */
+    public function minCoverSubStr( $s, $t ) {
+
+        $sourceLen = strlen($s);
+        $targetLen = strlen($t);
+
+        $window = [];
+        $need = [];
+
+        for ($i = 0; $i < $targetLen; $i++) {
+            $need[$t[$i]]++;
+        }
+        $elemLen  = count($need);
+
+        $left = $right = 0;
+
+        $valid = 0;
+        $start = 0;
+        $len = PHP_INT_MAX;
+
+        while ($right < $sourceLen) {
+            $c = $s[$right];
+            $right++; //窗口右移
+            if(isset($need[$c])) { //窗口数据更新
+                $window[$c]++;
+                if($window[$c] == $need[$c]) {
+                    $valid++;
+                }
+            }
+
+            while ($valid == $elemLen) {
+                if($right - $left < $len) {
+                    $len = $right - $left;
+                    $start = $left;
+                }
+                $d = $s[$left];
+                $left++;
+                if(isset($need[$d])) {
+                    if($window[$d] == $need[$d]) {
+                        $valid--;
+                    }
+                    $window[$d]--;
+                }
+            }
+        }
+
+        if($len == PHP_INT_MAX) {
+            return '';
+        } else {
+            return substr($s,$start,$len);
+        }
+
+    }
+
+    /**
+     * 字符串排列:给定两个字符串s1和s2,写一个函数来判断s2是否包含s1的全排列
+     * @param $s
+     * @param $t
+     * @return bool
+     */
+    public function checkIsPaiLie($s, $t) {
+
+        $targetLen = strlen($t);
+        $sourceLen = strlen($s);
+
+        $need = [];
+        $window = [];
+        for ($i = 0; $i < $targetLen; $i++) {
+            $need[$t[$i]]++;
+        }
+
+        $elemLen = count($need);
+
+        $left = $right = 0;
+
+        $valid = 0;
+        $start = 0;
+        $len = PHP_INT_MAX;
+
+        while ($right < $sourceLen) {
+
+            $c = $s[$right];
+            $right++;
+
+            if(isset($need[$c])) {
+                $window[$c]++;
+                if($window[$c] == $need[$c]) {
+                    $valid++;
+                }
+            }
+
+            if($right - $left == $targetLen) { //窗口数据和目标串大小一致时
+
+                if($valid == $elemLen) { //找到合法子串
+                    return true;
+                }
+
+                $d = $s[$left];
+                $left++;
+                if(isset($need[$d])) {
+                    if($need[$d] == $window[$d]) {
+                        $valid--;
+                    }
+                    $window[$d]--;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * 字母异位词：给定一个字符串s和一个非空字符串p,找到s中所有是p的字母异位词的子串,返回起始索引
+     * @param $s
+     * @param $t
+     * @return array
+     */
+    public function getDiffPos( $s, $t ) {
+
+        $sourceLen = strlen($s);
+        $targetLen = strlen($t);
+
+        $need = [];
+        $window = [];
+        for ($i = 0; $i < $targetLen; $i++) {
+            $need[$t[$i]]++;
+        }
+
+        $elemLen = count($need);
+
+        $left = $right = 0;
+        $valid = 0;
+        $result = [];
+
+        while ($right < $sourceLen) {
+            $c = $s[$right];
+            $right++;
+            if(isset($need[$c])) {
+                $window[$c]++;
+                if($window[$c] == $need[$c]) {
+                    $valid++;
+                }
+            }
+            while($right - $left >= $targetLen) {
+                if($valid == $elemLen) {
+                    $result[] = $left;
+                }
+                $d = $s[$left];
+                $left++;
+
+                if(isset($need[$d])) {
+                    if($window[$d] == $need[$d]) {
+                        $valid--;
+                    }
+                    $window[$d]--;
+                }
+            }
+
+        }
+        return $result;
+    }
+
+
+    /**
+     * 最长无重复子串
+     * @param $s
+     * @return bool|string
+     */
+    public function lengthOfLongestSubString($s) {
+
+        $window = [];
+        $sourceLen = strlen($s);
+
+        $left = $right = 0;
+        $maxLen = 0;
+        $start = 0;
+
+        while ($right < $sourceLen) {
+            $c = $s[$right];
+            $right++;
+            $window[$c]++;
+
+            while ($window[$c] > 1) {
+                $d = $s[$left];
+                $window[$d]--;
+                $left++;
+            }
+
+            if($right-$left > $maxLen) {
+                $start = $left;
+                $maxLen = $right-$left;
+            }
+
+        }
+
+        return substr($s,$start,$maxLen);
+    }
+
+
+}
+
+$obj = new window();
+$s = 'ADOBECCODEBANC';
+$t = 'ABC';
+//echo $obj->minCoverSubStr($s, $t) . PHP_EOL;
+
+//$s = "eidbaooo";
+//$s = "eidboaoo";
+//$t = "ab";
+//$res = $obj->checkIsPaiLie($s,$t);
+//if($res) {
+//    echo "匹配OK".PHP_EOL;
+//}else {
+//    echo "匹配不OK".PHP_EOL;
+//}
+
+//$s = "cbaebabacd";
+//$t = "abc";
+//$res = $obj->getDiffPos($s,$t);
+//foreach ($res ?:[] as $v) {
+//    echo $v.' ';
+//}
+//echo PHP_EOL;
+
+//$s = "abcabcbb";
+//$s = "bbbbb";
+$s = "pwwkew";
+$len = $obj->lengthOfLongestSubString($s);
+echo $len.PHP_EOL;
